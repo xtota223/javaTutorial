258fade56565abbd477a040dc7d1d4bc550df7af
Shuhrat Faiziev <fayziev.shuhrat@gmail.com>
2020-05-12 21:02:34 -0400

    hello word

---
+++ src/main/java/com/sheroz/Lesson1.java
@@ -0,0 +1,19 @@
+package com.sheroz;
+
+public class Lesson1 {
+
+
+  public static void main(String[] args) {
+
+    print("Hello world");
+    print("Hello world");
+
+
+  }
+
+  public static void print(String st  ) {
+    System.out.println(st); //  just print
+
+  }
+
+}
