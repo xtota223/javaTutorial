13669b320eb919eb3d477937a85a3e3547cd7aa2
xtota <doomdota67@gmail.com>
2020-05-19 22:12:20 -0400

    q

---
+++ src/main/java/com/sheroz/thingslike.java
@@ -0,0 +1,12 @@
+package com.sheroz;
+
+public class thingslike {
+    public static void main(String args[]) //fixed  (there was an error)
+    {
+        System.out.println ("chocolate, royalties, sleep");  //fixed (there was an error)
+    }
+    {
+        mySalary = 1000000.00;      //and one more error
+    }
+
+}
