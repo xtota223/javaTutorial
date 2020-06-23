def38df97c2ee65d6cb74b6256ce2db8e4352888
xtota <doomdota67@gmail.com>
2020-06-23 19:54:53 -0400

    q

---
+++ src/main/java/com/sheroz/Puzzle4.java
@@ -0,0 +1,20 @@
+package com.sheroz;
+
+public class Puzzle4 {
+    public  static void main(String [] args) {
+
+        int y = 1;
+        int x = 0;
+        int result = 0;
+        while (x < 6) {
+            Puzzle4[] obs = new Puzzle4[6];
+
+            y = y * 10;
+
+        }
+        x = 6;
+        while (x > 0);
+
+        }
+    }
+}
