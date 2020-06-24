320293d5c5dac42daeb8059166a274d757d7cf9e
Shuhrat Faiziev <fayziev.shuhrat@gmail.com>
2020-06-23 22:51:45 -0400

    lesson N4

--- src/main/java/com/sheroz/Puzzle4.java
+++ src/main/java/com/sheroz/Puzzle4.java
@@ -2,20 +2,39 @@ package com.sheroz;
 
 public class Puzzle4 {
     public  static void main(String [] args) {
-
+        Puzzle4b[] obs= new Puzzle4b[6];
         int y = 1;
         int x = 0;
         int result = 0;
         while (x < 6) {
-            Puzzle4[] obs = new Puzzle4[6];
+//            Puzzle4[] obs = new Puzzle4[6];
+            obs[x]= new Puzzle4b();
+            obs[x].ivar=y;
 
             y = y * 10;
+            x=x+1;
 
         }
         x = 6;
-        while (x > 0);
-        
+        while (x > 0){
+        x=x-1;
+        result=result+ obs[x].doStauff(x);
 
         }
+        System.out.println("result :" + result);
     }
+
+}
+class Puzzle4b {
+    int ivar;
+    public int doStauff(int factor ) {
+        if (ivar>100) {
+            return ivar * factor;
+        }
+        else {
+            return ivar*(2+ factor );
+        }
+
+    }
+
 }
