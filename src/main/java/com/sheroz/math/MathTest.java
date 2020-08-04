7ec0ffd97489358554efdd965aa86863252cfdda
Shuhrat Faiziev <fayziev.shuhrat@gmail.com>
2020-08-03 20:38:53 -0400

    lesson N5  object list

---
+++ src/main/java/com/sheroz/math/MathTest.java
@@ -0,0 +1,13 @@
+package com.sheroz.math;
+
+public class MathTest {
+
+  public static void main(String[] args) {
+
+    System.out.println ( "MAx number is : "  + Math.max(3,5  ) );
+    System.out.println( "random : " + (int) (Math.random() *100));
+    System.out.println( Math.round(9.565));
+
+  }
+
+}
