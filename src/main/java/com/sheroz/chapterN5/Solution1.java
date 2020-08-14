86f5ce090e66ae5823ea14915bbf2a34fbd04c0e
xtota <doomdota67@gmail.com>
2020-08-14 19:51:39 -0400

    qvvv

--- src/main/java/com/sheroz/chapterN5/Solution1.java
+++ src/main/java/com/sheroz/chapterN5/Solution1.java
@@ -1,6 +1,18 @@
 package com.sheroz.chapterN5;
-
+import java.util.Scanner;
 public class Solution1 {
-  //https://www.hackerrank.com/challenges/java-loops-i/problem
 
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 1;i<=10; i++){
+
+        int result = n*i;
+
+        System.out.println(n + " x " + i +  " = " + result);}
+    }
 }
+//complete
--- src/main/java/com/sheroz/chapterN5/Solution2.java
+++ src/main/java/com/sheroz/chapterN5/Solution2.java
@@ -1,6 +1,18 @@
 package com.sheroz.chapterN5;
+import java.util.*;
 
 public class Solution2 {
- // https://www.hackerrank.com/challenges/java-loops/problem
+ public static void main(String[]args){
+
+     Scanner in = new Scanner(System.in);
+
+     int a =in.nextInt();
+     int b =in.nextInt();
+     int n =in.nextInt();
+
+
+
+ }
 
 }
+// https://www.hackerrank.com/challenges/java-loops/problem
