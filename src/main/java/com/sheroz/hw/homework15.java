e8a38f090510f0e9d3efa44b9d417ca6b0f24be5
xtota <doomdota67@gmail.com>
2020-08-20 20:27:17 -0400

    qvvv

--- src/main/java/com/sheroz/hw/homework15.java
+++ src/main/java/com/sheroz/hw/homework15.java
@@ -1,4 +1,12 @@
 package com.sheroz.hw;
-
 public class homework15 {
+    public static void main(String[]args){
+        double a =1, sum=0;
+        for (int x=1;x<=10;x++) {
+        double f =a/x;
+        sum +=f;
+            System.out.println(f);
+        }
+        System.out.println("The sum is "+sum);
+    }
 }
