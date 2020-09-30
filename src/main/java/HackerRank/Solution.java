76b55b2b4542d8fee52e008fa7141b8f8096eda5
xtota <doomdota67@gmail.com>
2020-09-29 20:36:34 -0400

    qvvv

--- src/main/java/HackerRank/Solution.java
+++ src/main/java/HackerRank/Solution.java
@@ -4,13 +4,9 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int i = scan.nextInt();
-        double d =scan.nextDouble();
-        String s = scan.nextLine();
-
-        System.out.println("String: " + s);
-        System.out.println("Double: " + d);
-        System.out.println("Int: " + i);
+        float i = scan.nextFloat();
+    double inp = i/3;
+        System.out.println(inp);
     }
 }
 
--- src/main/java/com/sheroz/chapter42/NumberRatio.java
+++ src/main/java/com/sheroz/chapter42/NumberRatio.java
@@ -1,6 +1,40 @@
 package com.sheroz.chapter42;
+import java.util.Scanner;
+//https://www.hackerrank.com/challenges/plus-minus/problem
 
 public class NumberRatio {
-  //https://www.hackerrank.com/challenges/plus-minus/problem
+static Scanner sc = new Scanner(System.in);
+public static void main (String []args){
+
+    int[] arr = new int[5];
+
+    for(int i=0;i<5;i++) {
+        arr[i] = sc.nextInt();
+    }
+    int poz=0;
+    int neg=0;
+    int zer=0;
+
+    for(int i=0;i<5;i++) {
+        if (arr[i]>0)
+            poz+=1;
+        else if (arr[i]<0)
+            neg+=1;
+        else if (arr[i]==0)
+            zer+=1;
+    }
+    double pozf=(poz/arr.length);
+    double negf=(neg/arr.length);
+    double zerf=(zer/arr.length);
+
+    System.out.println(pozf+"\n"+negf+"\n"+zerf);
+
+    }
 
 }
+//   System.out.println("there is: "+poz+" positive numbers.\n"+
+//                     "there is: "+neg+" negative numbers.\n"+
+//                    "there is: "+zer+" zero numbers.\n");
+
+
+
