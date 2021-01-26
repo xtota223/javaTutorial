a0d1e9a70ebc4ee38d4c663c0136a1870f632fc4
Shuhrat Faiziev <fayziev.shuhrat@gmail.com>
2021-01-25 20:20:33 -0500

    Hacker Rank  array  list

--- src/main/java/com/sheroz/chapter43/Solution.java
+++ src/main/java/com/sheroz/chapter43/Solution.java
@@ -2,35 +2,36 @@ package com.sheroz.chapter43;
 
 //https://www.hackerrank.com/challenges/java-arraylist/problem
 
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+
 
 public class Solution {
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     int size =  scanner.nextInt();
-    List<List<String>> arrayList2d= new ArrayList<>();
+    List<List<Integer>> arrayList2d= new ArrayList<>();
     scanner.nextLine();
-    for (int i=0 ; i<size; i++){
-      arrayList2d.add(new ArrayList<String>());
-      for (String st : scanner.nextLine().split(" ")) {
-        arrayList2d.get(i).add(st);
+
+
+    for (int j=0 ; j<size; j++) {
+      int jArraySize=  scanner.nextInt();
+      arrayList2d.add(j, new ArrayList<>());
+      for (int i=0 ; i<jArraySize; i++) {
+        arrayList2d.get(j).add(scanner.nextInt());
       }
-
-
     }
 
+    System.out.println("++++++++++++");
     for (int i=0 ; i<size; i++){
-      for (String str : arrayList2d.get(i)){
+      for (Integer str : arrayList2d.get(i)){
         System.out.print( str + " " );
       }
       System.out.println();
 
     }
+
+    System.out.println( "first : " +  arrayList2d.get(1-1).get(3-1) );
 
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
